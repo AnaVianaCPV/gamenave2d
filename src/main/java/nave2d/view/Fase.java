c6e7210514b1bc437730d04eb5c28b1cb363cc4a
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-12-26 17:42:18 -0300

    Interface fundo preto

--- src/main/java/nave2d/view/Fase.java
+++ src/main/java/nave2d/view/Fase.java
@@ -6,16 +6,23 @@ import java.awt.*;
 public class Fase extends JPanel {
     private Image fundo;
 
-    public Fase(){
-        ImageIcon referencia = new ImageIcon("");
-        fundo = referencia.getImage();
-
+    public Fase() {
+        try {
+            ImageIcon referencia = new ImageIcon(getClass().getResource("/image/fundopreto.jpg"));
+            fundo = referencia.getImage();
+        } catch (Exception e) {
+            System.err.println("Erro ao carregar imagem: " + e.getMessage());
+        }
     }
 
-    public void paint(Graphics g){
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
         Graphics2D graficos = (Graphics2D) g;
         graficos.drawImage(fundo, 0, 0, null);
-        g.dispose();
 
+        if (fundo != null) {
+            graficos.drawImage(fundo, 0, 0, getWidth(), getHeight(), null);
+        }
     }
 }
