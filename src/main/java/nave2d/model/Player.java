a1ad8901a6043204542a97862af220ab0d85c8de
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-12-26 17:56:29 -0300

    Player

---
+++ src/main/java/nave2d/model/Player.java
@@ -0,0 +1,17 @@
+package nave2d.model;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Player {
+    private int x, y;
+    private int dx, dy; // Deslocamento (velocidade atual)
+    private Image imagem;
+    private int altura, largura;
+
+    public Player(){
+        this.x = 100;
+        this.y = 100;
+    }
+
+}
