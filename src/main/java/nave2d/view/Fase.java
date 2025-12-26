60532929ab7759bf9b856d29c87070abe9fb323f
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-12-26 17:08:24 -0300

    Interface

---
+++ src/main/java/nave2d/view/Fase.java
@@ -0,0 +1,21 @@
+package nave2d.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Fase extends JPanel {
+    private Image fundo;
+
+    public Fase(){
+        ImageIcon referencia = new ImageIcon("");
+        fundo = referencia.getImage();
+
+    }
+
+    public void paint(Graphics g){
+        Graphics2D graficos = (Graphics2D) g;
+        graficos.drawImage(fundo, 0, 0, null);
+        g.dispose();
+
+    }
+}
--- src/main/java/nave2d/view/Tela.java
+++ src/main/java/nave2d/view/Tela.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class Tela extends JFrame {
     public Tela(){
+        add(new Fase());
         setTitle("Nave 2D");
         setSize(1024,728);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
